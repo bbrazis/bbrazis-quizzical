e1caafeaf6ceede91bd551516aaaa69b05341f17
bbrazis <web@broncowine.com>
2024-05-28 09:26:51 -0700

    Update index.jsx

--- index.jsx
+++ index.jsx
@@ -1,6 +1,22 @@
 import React from "react"
 import ReactDOM from "react-dom/client"
 import App from './App'
+const root = document.getElementById('root')
 
+ReactDOM.createRoot(root).render(<App />);
 
-ReactDOM.createRoot(document.getElementById('root')).render(<App />);
+/* detect if using dark mode theme */
+if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+    root.querySelectorAll('.app')[0].classList.add('dark-mode')
+}
+
+/* watching for theme changes */
+window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', event => {
+    const newColorScheme = event.matches ? "dark" : "light";
+    
+    if(newColorScheme === "dark") {
+        root.querySelectorAll('.app')[0].classList.add('dark-mode')
+    } else {
+	    root.querySelectorAll('.app')[0].classList.remove('dark-mode')
+    }
+});
