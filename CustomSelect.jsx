0d0d24769c23adc82fe60c0b313a0ecc7aeefe97
bbrazis <bradley.brazis@gmail.com>
2024-06-04 10:43:51 -0700

    Trying something
    I'm trying to get this working by copying the original code and seeing if my code was wrong.

--- CustomSelect.jsx
+++ CustomSelect.jsx
@@ -1,5 +1,6 @@
-import React from 'react'
+import React, { useEffect, useRef, useState } from "react"
 
+// Icon component
 const Icon = ({ isOpen }) => {
     return (
         <svg viewBox="0 0 24 24" width="18" height="18" stroke="#222" strokeWidth="1.5" fill="none" strokeLinecap="round" strokeLinejoin="round" className={isOpen ? 'translate' : ''}>
@@ -8,7 +9,7 @@ const Icon = ({ isOpen }) => {
     )
 }
 
-// CloseIcon
+// CloseIcon component
 const CloseIcon = () => {
     return (
         <svg viewBox="0 0 24 24" width="14" height="14" stroke="#fff" strokeWidth="2" fill="none" strokeLinecap="round" strokeLinejoin="round">
@@ -17,26 +18,32 @@ const CloseIcon = () => {
     )
 }
 
-export default function({ placeHolder, options, isMulti, isSearchable, onChange, align }) {
-    const [showMenu, setShowMenu] = React.useState(false) // Controls the visibility of the dropdown menu
-    const [selectedValue, setSelectedValue] = React.useState(isMulti ? [] : null) // Stores the selected value(s)
-    const [searchValue, setSearchValue] = React.useState('') // Stores the value entered in the search input
-    const searchRef = React.useRef() // Reference to the search input element
-    const inputRef = React.useRef() // Reference to the custom select input element
+// CustomSelect component
+const CustomSelect = ({ placeHolder, options, isMulti, isSearchable, onChange, align }) => {
+    // State variables using React hooks
+    const [showMenu, setShowMenu] = useState(false) // Controls the visibility of the dropdown menu
+    const [selectedValue, setSelectedValue] = useState(isMulti ? [] : null) // Stores the selected value(s)
+    const [searchValue, setSearchValue] = useState("") // Stores the value entered in the search input
+    const searchRef = useRef() // Reference to the search input element
+    const inputRef = useRef() // Reference to the custom select input element
 
-    React.useEffect(()=>{
-        setSearchValue('')
-        if(showMenu && searchRef.current) searchRef.current.focus()
-    },[showMenu])
+    useEffect(() => {
+        setSearchValue("")
+        if (showMenu && searchRef.current) {
+            searchRef.current.focus()
+        }
+    }, [showMenu])
 
-    React.useEffect(()=>{
+    useEffect(() => {
         const handler = (e) => {
-            if(inputRef.current && !inputRef.current.contains(e.target)) setShowMenu(false)
+            if (inputRef.current && !inputRef.current.contains(e.target)) {
+                setShowMenu(false)
+            }
         }
 
-        window.addEventListener('click', handler)
+        window.addEventListener("click", handler)
         return () => {
-            window.removeEventListener('click', handler)
+            window.removeEventListener("click", handler)
         }
     })
 
@@ -45,15 +52,17 @@ export default function({ placeHolder, options, isMulti, isSearchable, onChange,
     }
 
     const getDisplay = () => {
-        if(!selectedValue || selectedValue.length === 0) placeHolder
-        if(isMulti){
+        if (!selectedValue || selectedValue.length === 0) {
+            return placeHolder
+        }
+        if (isMulti) {
             return (
                 <div className="dropdown-tags">
                     {
-                        selectedValue.map((option,index) => (
+                        selectedValue.map((option, index) => (
                             <div key={`${option.value}-${index}`} className="dropdown-tag-item">
                                 {option.label}
-                                <span onClick={(e) => onTagRemove(e, option)} className="dropdown-tag-close">
+                                <span onClick={(e) => onTagRemove(e, option)} className="dropdown-tag-close" >
                                     <CloseIcon />
                                 </span>
                             </div>
@@ -65,7 +74,9 @@ export default function({ placeHolder, options, isMulti, isSearchable, onChange,
         return selectedValue.label
     }
 
-    const removeOption = (option) => selectedValue.filter((o) => o.value !== option.value)
+    const removeOption = (option) => {
+        return selectedValue.filter((o) => o.value !== option.value)
+    }
 
     const onTagRemove = (e, option) => {
         e.stopPropagation()
@@ -76,70 +87,80 @@ export default function({ placeHolder, options, isMulti, isSearchable, onChange,
 
     const onItemClick = (option) => {
         let newValue
-        if(isMulti) {
-            if(selectedVlaue.findIndex((o) => o.value === option.value) >= 0) {
+        if (isMulti) {
+            if (selectedValue.findIndex((o) => o.value === option.value) >= 0) {
                 newValue = removeOption(option)
-            } else newValue = [...selectedValue, option]
-        } else newValue = option
+            } else {
+                newValue = [...selectedValue, option]
+            }
+        } else {
+            newValue = option
+        }
         setSelectedValue(newValue)
         onChange(newValue)
     }
 
     const isSelected = (option) => {
-        if(isMulti) {
-            return selectedValue.filter((o) => o.value === option.value).length
+        if (isMulti) {
+            return selectedValue.filter((o) => o.value === option.value).length > 0
         }
 
-        if(!selectedValue) false
+        if (!selectedValue) {
+            return false
+        }
 
         return selectedValue.value === option.value
     }
 
-    const onSearch = (e) => setSearchValue(e.target.value)
+    const onSearch = (e) => {
+        setSearchValue(e.target.value)
+    }
 
     const getOptions = () => {
-        if(!searchValue) options
+        if (!searchValue) {
+            return options
+        }
 
-        return options.filter((option) =>
-            option.label.toLowerCase().indexOf(searchValue.toLowerCase()) >= 0
+        return options.filter(
+            (option) =>
+                option.label.toLowerCase().indexOf(searchValue.toLowerCase()) >= 0
         )
     }
 
-    const optionList = () => {
-        options.map((option) => (
-            <div onClick={() => onItemClick(option)} key={option.value} className={`dropdown-item ${isSelected(option) && "selected"}`} >
-                {option.label}
-            </div>
-        ))
-    }
-
-    const Menu = () => (
-        <div className={`dropdown-menu alignment--${align || 'auto'}`}>
-            {
-                isSearchable && (
-                    <div className="search-box">
-                        <input className="form-control" onChange={onSearch} value={searchValue} ref={searchRef} />
-                    </div>
-                )
-            }
-            {optionList}
-        </div>
-    )
-
-    console.log(optionList)
     return (
         <div className="custom--dropdown-container">
-            {
-                <div ref={inputRef} onClick={handleInputClick} className="dropdown-input">
-                    <div className={`dropdown-selected-value ${!selectedValue || selectedValue.length === 0 ? 'placeholder' : ''}`}>
-                        {getDisplay()}
-                    </div>
+
+            <div ref={inputRef} onClick={handleInputClick} className="dropdown-input">
+                <div className={`dropdown-selected-value ${!selectedValue || selectedValue.length === 0 ? 'placeholder' : ''}`}>{getDisplay()}</div>
+                <div className="dropdown-tools">
                     <div className="dropdown-tool">
                         <Icon isOpen={showMenu} />
                     </div>
                 </div>
+            </div>
+
+            {
+                showMenu && (
+                    <div className={`dropdown-menu alignment--${align || 'auto'}`}>
+                        {
+                            isSearchable && (
+                                <div className="search-box">
+                                    <input className="form-control" onChange={onSearch} value={searchValue} ref={searchRef} />
+                                </div>
+                            )
+                        }
+                        {
+                            getOptions().map((option) => (
+                                <div onClick={() => onItemClick(option)} key={option.value} className={`dropdown-item ${isSelected(option) && "selected"}`} >
+                                    {option.label}
+                                </div>
+                            ))
+                        }
+                    </div>
+                )
             }
-            {Menu}
         </div>
     )
 }
+
+export default CustomSelect
